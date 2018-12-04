7b7a58841eb981bcfbe9b9abda8e77b0f7215921
kuzuro <kuzuro@gmail.com>
2018-12-04 15:33:20 +0900

    Add files via upload

---
+++ nextprev title view/nextprev_title_view.js
@@ -0,0 +1,19 @@
+﻿<b:if cond='data:blog.pageType == &quot;item&quot;'>
+  <script>
+  //<![CDATA[
+      var newerLink = $("a.blog-pager-newer-link");
+      var olderLink = $("a.blog-pager-older-link");
+
+      $.get(newerLink.attr("href"), function(newer){
+          newerLink.parent().append("<div class='next-title'>"+$(newer).find("h3.post-title").text()+"</div>");
+
+		  newerLink.parent().append("<img src='"+$(newer).find(".post-body img").eq(1).attr("src")+"'>");
+
+      },"html");
+
+      $.get(olderLink.attr('href'), function(older){
+          olderLink.parent().append("<div class='prev-title'>"+$(older).find("h3.post-title").text()+"</div>");
+      },"html");
+  //]]>
+  </script>
+</b:if>
